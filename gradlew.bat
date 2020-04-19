0e4327b2f6ab8eae47b0ce06480a561f0fad857e
Wang wei <429744781@qq.com>
2020-04-19 20:26:04 +0800

    提交

--- gradlew.bat
+++ gradlew.bat
@@ -30,7 +30,8 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set GRADLE_OPTS=-XX:MaxMetaspaceSize=2048m -Xmx2048m -XX:MaxHeapSize=1024m %GRADLE_OPTS%
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
